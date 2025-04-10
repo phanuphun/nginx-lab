4600c4d4d055c97f8b9e2eb6f48d8aa5245f552a
phanuphun <parnuphun1598@gmail.om>
2025-04-10 15:39:54 +0700

    lab 3 reverse proxy and load balance

---
+++ 3.reverse-proxy-and-load-balancing/backend2/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend server 2');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
